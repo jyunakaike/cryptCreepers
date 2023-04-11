a757440ff3e8138dfc68aad6b7430527b331cbfa
jyun <jyunakaike@gmail.com>
2023-04-11 15:11:29 -0400

    create powerup

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -7,12 +7,15 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] GameObject checkpointPrefab; 
     [SerializeField] int checkpointSpawnDelay = 10;
     [SerializeField] float spawnRadius = 6;
+    [SerializeField] GameObject[] powerUpPrefab; 
+    [SerializeField]int powerUpSpawnDelay = 12;
 
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnCheckpointRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
     }
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
@@ -25,6 +28,15 @@ public class ItemSpawner : MonoBehaviour
             yield return new WaitForSeconds(checkpointSpawnDelay);
             Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
             Instantiate(checkpointPrefab, randomPosition, Quaternion.identity);
+        }
+    }
+    IEnumerator SpawnPowerUpRoutine(){
+        while (true)
+        {
+            yield return new WaitForSeconds(powerUpSpawnDelay);
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+            int random = Random.Range(0, powerUpPrefab.Length);
+            Instantiate(powerUpPrefab[random], randomPosition , Quaternion.identity);
         }
     }
 }
