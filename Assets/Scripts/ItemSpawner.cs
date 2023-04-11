21af7e2cc4c394d8fea53037eb79670f1add6952
jyun <jyunakaike@gmail.com>
2023-04-10 21:35:19 -0400

    gizmo show radio

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,13 +6,17 @@ public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] GameObject checkpointPrefab; 
     [SerializeField] int checkpointSpawnDelay = 10;
-    [SerializeField] float spawnRadius = 10;
+    [SerializeField] float spawnRadius = 6;
 
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnCheckpointRoutine());
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 
     IEnumerator SpawnCheckpointRoutine(){
